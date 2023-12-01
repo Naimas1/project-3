8ceda3151049f287e893dc3aa5c0d7a820e8fcb8
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-12-01 19:49:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,61 @@
-﻿namespace project_3
+using System;
+using System.Data.SqlClient;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string connectionString = " "; 
+
+        // Створення таблиці "Products"
+        string createTableQuery = @"
+            CREATE TABLE Products (
+                ProductID INT PRIMARY KEY IDENTITY(1,1),
+                ProductName NVARCHAR(255) NOT NULL,
+                ProductType NVARCHAR(255) NOT NULL,
+                Supplier NVARCHAR(255) NOT NULL,
+                Quantity INT NOT NULL,
+                Cost DECIMAL(18, 2) NOT NULL,
+                SupplyDate DATE NOT NULL
+            )";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            Console.WriteLine("Hello, World!");
+            connection.Open();
+
+            using (SqlCommand command = new SqlCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("Таблиця 'Products' створена.");
+
+            // Вставка даних
+            InsertProduct(connection, "Лаптоп", "Електроніка", "ABC Електроніка", 10, 1200.50m, DateTime.Now);
+            InsertProduct(connection, "Книга", "Книги", "Книгарня 'Читай-Гарно'", 50, 15.99m, DateTime.Now.AddDays(-30));
+         
         }
+    }
+
+    static void InsertProduct(SqlConnection connection, string productName, string productType, string supplier,
+                              int quantity, decimal cost, DateTime supplyDate)
+    {
+        string insertDataQuery = @"
+            INSERT INTO Products (ProductName, ProductType, Supplier, Quantity, Cost, SupplyDate)
+            VALUES (@ProductName, @ProductType, @Supplier, @Quantity, @Cost, @SupplyDate)";
+
+        using (SqlCommand command = new SqlCommand(insertDataQuery, connection))
+        {
+            command.Parameters.AddWithValue("@ProductName", productName);
+            command.Parameters.AddWithValue("@ProductType", productType);
+            command.Parameters.AddWithValue("@Supplier", supplier);
+            command.Parameters.AddWithValue("@Quantity", quantity);
+            command.Parameters.AddWithValue("@Cost", cost);
+            command.Parameters.AddWithValue("@SupplyDate", supplyDate);
+
+            command.ExecuteNonQuery();
+        }
+
+        Console.WriteLine($"Дані для товару '{productName}' вставлені.");
     }
 }
